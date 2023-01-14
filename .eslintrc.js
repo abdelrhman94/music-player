833f9e2a5660a92782d86545b580b7be58486821
Abdelrhman Farag <abdelrhman.s.farag@gmail.com>
2023-01-14 16:39:32 +0200

    custmize eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,6 +44,7 @@ module.exports = {
     'jsx-a11y/no-autofocus': 0,
     'jsx-a11y/no-static-element-interactions': 0,
     'react/no-array-index-key': 0,
+    'jsx-quotes': ['error', 'prefer-single'],
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
